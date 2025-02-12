9817e65418fb1e32ebd18cb402316d89040a26ec
GIRI-R-S <giri.srinivasa.r@gmail.com>
2025-02-12 14:07:26 +0530

    Add files via upload

---
+++ typeDef.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class typeDef
+{
+	public static void main(String[] args) {
+	    byte a=78;
+	    byte b=20;
+	    byte c=(byte)(a*b);
+		System.out.println(c);
+		System.out.println(a/b);
+		System.out.println((byte)(a*b));
+		System.out.println(a-b);	
+	}
+}
